12f18db5c7ce5cfa57e083cf3e15332876f45084
MyrasYa <rasyaakbar2310@gmail.com>
2026-06-05 00:05:17 +0700

    feat: implement task CRUD REST API

--- src/main/java/com/rasya/todo_app/controller/TaskController.java
+++ src/main/java/com/rasya/todo_app/controller/TaskController.java
@@ -10,6 +10,9 @@ import com.rasya.todo_app.service.TaskService;
 // import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -26,11 +29,29 @@ public class TaskController {
         return taskService.getAllTasks();
     }
 
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+
     @PostMapping()
     public Task createTask(@RequestBody Task task) {
         //TODO: process POST request
         
         return taskService.saveTask(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTask(@PathVariable Long id) 
+    {
+        taskService.deleteTask(id);
+
+        return "Task berhasil dihapus!";
     }
     
     
--- src/main/java/com/rasya/todo_app/service/TaskService.java
+++ src/main/java/com/rasya/todo_app/service/TaskService.java
@@ -18,9 +18,31 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    public Task updateTask(Long id, Task newTask) {
+        Task task = taskRepository.findById(id).orElse(null);
+
+        if (task != null) {
+            task.setTitle(newTask.getTitle());
+            task.setDescription(newTask.getDescription());
+            task.setCompleted(newTask.isCompleted());
+
+            return taskRepository.save(task);
+
+        }
+
+        return null;
+
+    }
+
     public Task saveTask(Task task) {
         return taskRepository.save(task);
     }
 
-    
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
 }
